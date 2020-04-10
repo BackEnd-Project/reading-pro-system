2c56a9107bd2003f372d8b9743730787fac0d9bc
Freedom <1047807786@qq.com>
2020-04-10 10:29:56 +0800

    跨域请求设置

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,12 +15,20 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        return $next($request);
         $response = $next($request);
-        $response->header('Access-Control-Allow-Origin', '*');
-        $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, Accept, multipart/form-data, application/json');
-        $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
-        $response->header('Access-Control-Allow-Credentials', 'false');
+        $origin = $request->server('HTTP_ORIGIN') ? $request->server('HTTP_ORIGIN') : '';
+        $allow_origin = [
+            'http://192.168.23.1:8080',
+            'http://122.114.197.99:9001',
+        ];
+        if (in_array($origin, $allow_origin)) {
+            $response->header('Access-Control-Allow-Origin', $origin);
+            $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, Accept, multipart/form-data, application/json， X-CSRF-TOKEN, Accept, Authorization, X-XSRF-TOKEN');
+            $response->header('Access-Control-Expose-Headers', 'Authorization, authenticated');
+            $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+            $response->header('Access-Control-Allow-Credentials', 'true');
+        }
+
         return $response;
     }
 }
